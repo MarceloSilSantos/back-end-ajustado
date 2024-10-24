2f9acfd2041b57c01e1c03d2d4e6ee39db4ec8ab
MarceloSilSantos <tecmarcelo78@outlook.com>
2024-10-23 22:25:48 -0300

    spring boot base

---
+++ src/main/java/br/dev/onepiece/webservicepiece/WebservicepieceApplication.java
@@ -0,0 +1,13 @@
+package br.dev.onepiece.webservicepiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicepieceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicepieceApplication.class, args);
+	}
+
+}
